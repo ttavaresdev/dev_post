34747486c21f3fbf366b1e83a44cfc7e909478c8
tstavares <ttavares.develop@gmail.com>
2022-06-08 16:56:46 -0400

    posts conteudo em alta

--- src/components/FavoritePost/index.js
+++ src/components/FavoritePost/index.js
@@ -1,56 +1,60 @@
 import React from 'react'
-import {View, Text, TouchableOpacity, StyleSheet, ImageBackground, Dimensions} from 'react-native'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  Dimensions
+} from 'react-native'
 
 import { useNavigation } from '@react-navigation/native'
 
-const{width: WIDTH} = Dimensions.get('window')
+const { width: WIDTH } = Dimensions.get('window')
 
-export default function FavoritePost({data}){
-  const navigation = useNavigation();
-    function handleNavigate(){
-     navigation.navigate("Detail", {id: data.id})
-   }
-  return(
-    <TouchableOpacity 
-    style={styles.container}
-    onPress={handleNavigate}
-    >
-
-      <ImageBackground 
-      style={styles.cover}
-      resizeMode="cover"
-      source={{uri:`http://192.168.2.119:1337${data?.attributes?.cover?.data?.attributes?.url}` }}
-      blurRadius={3}
-      imageStyle={{ borderRadius: 6, opacity: 0.6}}
+export default function FavoritePost({ data }) {
+  const navigation = useNavigation()
+  function handleNavigate() {
+    navigation.navigate('Detail', { id: data.id })
+  }
+  return (
+    <TouchableOpacity style={styles.container} onPress={handleNavigate}>
+      <ImageBackground
+        style={styles.cover}
+        resizeMode="cover"
+        source={{
+          uri: `http://192.168.2.119:1337${data?.attributes?.cover?.data?.attributes?.url}`
+        }}
+        blurRadius={3}
+        imageStyle={{ borderRadius: 6, opacity: 0.6 }}
       >
-      <Text style={styles.title} numberOfLines={1}>
-        {data?.attributes?.title}
-      </Text>
+        <Text style={styles.title} numberOfLines={1}>
+          {data?.attributes?.title}
+        </Text>
       </ImageBackground>
     </TouchableOpacity>
   )
 }
-const styles = StyleSheet.create ({
-  container:{
-    felx:1,
-    marginRight: 8,
-
+const styles = StyleSheet.create({
+  container: {
+    felx: 1,
+    marginRight: 8
   },
-  cover:{
+  cover: {
     borderRadius: 4,
-    width: WIDTH -60,
+    width: WIDTH - 60,
     height: 100,
-    justifyContent:'flex-end',
-    backgroundColor:'#232630'
+    justifyContent: 'flex-end',
+    backgroundColor: '#232630'
   },
-  title:{
+  title: {
     fontSize: 17,
     fontWeight: 'bold',
     color: '#FFF',
-    paddingVertical:8,
-    paddingHorizontal:12,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
     textShadowColor: '#121212',
-    textShadowOffset: {width:2, height: 1},
-    textShadowRadius:8,
+    textShadowOffset: { width: 2, height: 1 },
+    textShadowRadius: 8
   }
 })
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,90 +1,108 @@
-import React, {useEffect, useState} from 'react'
-import { 
-  View, 
+import React, { useEffect, useState } from 'react'
+import {
+  View,
   Text,
   StyleSheet,
-  SafeAreaView, 
-  TouchableOpacity, 
-  FlatList 
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList
 } from 'react-native'
 
-import {useNavigation}from '@react-navigation/native'
-import{Feather} from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { Feather } from '@expo/vector-icons'
 import api from '../../services/api'
 
 import CategoryItem from '../../components/CategoryItem'
-import  {getFavorite, setFavorite } from '../../services/favorite'
+import { getFavorite, setFavorite } from '../../services/favorite'
 import FavoritePost from '../../components/FavoritePost'
+import PostItem from '../../components/PostItem'
 
 export default function Home() {
-  const navigation = useNavigation ();
+  const navigation = useNavigation()
   const [categories, setCategories] = useState([])
   const [favCategory, setFavCategory] = useState([])
 
+  const [posts, setPosts] = useState([])
+
   useEffect(() => {
-    async function loadData (){
-      const category = await api.get ("/api/categories?populate=icon")
+    async function loadData() {
+      await getListPosts()
+
+      const category = await api.get('/api/categories?populate=icon')
       setCategories(category.data.data)
     }
-    loadData();
-  },[])
+    loadData()
+  }, [])
 
-  useEffect(() =>{
-    async function favorite(){
-      const response = await getFavorite();
-      setFavCategory(response);
+  useEffect(() => {
+    async function favorite() {
+      const response = await getFavorite()
+      setFavCategory(response)
     }
-    favorite();
-  },[])
+    favorite()
+  }, [])
+  async function getListPosts() {
+    const response = await api.get(
+      'api/posts?populate=cover&sort=createdAt:desc'
+    )
+    setPosts(response.data.data)
+  }
   // Favoritando uma categoria
-  async function handleFavorite (id){
-    const response = await setFavorite(id);
-    setFavCategory(response);
+  async function handleFavorite(id) {
+    const response = await setFavorite(id)
+    setFavCategory(response)
   }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.name}> DevBlog</Text>
 
-        <TouchableOpacity onPress={() => navigation.navigate ("Search")}>
-          <Feather name ="search" size={24} color="#FFF"/>
+        <TouchableOpacity onPress={() => navigation.navigate('Search')}>
+          <Feather name="search" size={24} color="#FFF" />
         </TouchableOpacity>
       </View>
 
-     <FlatList
-      showsHorizontalScrollIndicator={false}
-      horizontal ={true}
-      contentContainerStyle={{paddingRight: 12}}
-      style={styles.categories}
-      data={categories}
-      keyExtractor={ (item) => String(item.id)}
-      renderItem={ ({item}) => (
-        <CategoryItem
-        data={item}
-        favorite={()=> handleFavorite(item.id)}
-        />
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        contentContainerStyle={{ paddingRight: 12 }}
+        style={styles.categories}
+        data={categories}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <CategoryItem data={item} favorite={() => handleFavorite(item.id)} />
         )}
-     />
+      />
 
       <View style={styles.main}>
-          {favCategory.length !== 0 && (
-            <FlatList 
-              style ={{ marginTop:50, maxHeight: 100, paddingStart: 18,}}
-              contentContainerStyle={{paddingEnd:25}}
-              data={favCategory}
-              horizontal={true}
-              showsHorizontalScrollIndicator={false}
-              keyExtractor={ (item) => String(item.id) }
-              renderItem={({item}) => <FavoritePost data={item}/>}
-            />
+        {favCategory.length !== 0 && (
+          <FlatList
+            style={{ marginTop: 50, maxHeight: 100, paddingStart: 18 }}
+            contentContainerStyle={{ paddingEnd: 25 }}
+            data={favCategory}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={item => String(item.id)}
+            renderItem={({ item }) => <FavoritePost data={item} />}
+          />
+        )}
 
-          )}
-          <Text style={[
+        <Text
+          style={[
             styles.title,
-            { marginTop: favCategory.length > 0 ? 14 : 35}
-            ]}>Conteúdo em Alta</Text>
-      </View>
+            { marginTop: favCategory.length > 0 ? 14 : 35 }
+          ]}
+        >
+          Conteúdo em Alta</Text>
 
+        <FlatList
+          style={{ flex: 1, paddingHorizontal: 18 }}
+          showsVerticalScrollIndicator={false}
+          data={posts}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <PostItem data={item}/>}
+        />
+      </View>
     </SafeAreaView>
   )
 }
@@ -93,7 +111,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#232630'
   },
-  header:{
+  header: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
@@ -101,35 +119,31 @@ const styles = StyleSheet.create({
     marginTop: 18,
     marginBottom: 18
   },
-  name:{
+  name: {
     color: '#FFF',
     fontSize: 28,
     fontWeight: 'bold'
-
   },
-  categories:{
+  categories: {
     maxHeight: 115,
     backgroundColor: '#EFEFEF',
-    marginHorizontal:18,
+    marginHorizontal: 18,
     borderRadius: 8,
     zIndex: 9
   },
-  main:{
-    flex:1,
-    backgroundColor:'#FFF',
+  main: {
+    flex: 1,
+    backgroundColor: '#FFF',
     marginTop: -30,
     borderTopLeftRadius: 12,
-    borderTopRightRadius: 12,
-    alignItems:'flex-start'
-    
+    borderTopRightRadius: 12
   },
-  title:{
-    fontSize:21,
-    fontWeight:'bold',
-    marginBottom:14,
-    fontFamily:'Roboto',
+  title: {
+    fontSize: 21,
+    fontWeight: 'bold',
+    marginBottom: 14,
+    fontFamily: 'Roboto',
     paddingHorizontal: 18,
-    color:"#162133"
-
+    color: '#162133'
   }
 })
